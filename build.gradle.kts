521bc2de18c30ed96fffcf471d5bab4d009662ee
antonio.petricca <antonio.petricca@fluttersea.com>
2026-03-25 21:03:22 +0100

    refactor(build): Adjust ideaVersion block for plugin configuration
    - Move ideaVersion block to ensure proper configuration.
    - Maintain compatibility with future builds by leaving untilBuild unset.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ val pluginGroup: String by project
 val pluginVersion: String by project
 val pluginName: String by project
 val pluginDescription: String by project
+val platformVersion: String by project
 val pluginSinceBuild: String by project
 val javaVersion: String by project
 val pluginChangeNotes: String by project
@@ -39,9 +40,9 @@ dependencies {
 
 intellijPlatform {
     pluginConfiguration {
-        ideaVersion {
         name        = pluginName
         description = pluginDescription
+        ideaVersion {
             sinceBuild = pluginSinceBuild
             // untilBuild is intentionally left unset to allow all future builds
         }
